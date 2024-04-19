3713f7ed91c43997ee6dda54352d4b96bda83a4f
DanilCh <chernishewdanil@gmail.com>
2024-04-19 20:59:06 +0300

    Complete task3

--- task3/task_3.py
+++ task3/task_3.py
@@ -1,26 +1,33 @@
 import json
 
 
-def update_values(tests, values):
-    for test in tests:
-        for value in values['values']:
-            if test['id'] == value['id']:
-                test['value'] = value['value']
-        if 'values' in test:
-            update_values(test, values)
-
-
-path_to_test = ('F:\\Python\\Code\\Test\\task3\\tests.json')
-path_to_values = ('F:\\Python\\Code\\Test\\task3\\values.json')
-
-try:
-    with open(path_to_test, 'r') as tests_file, open(path_to_values, 'r') as values_file:
-        tests_data = json.load(tests_file)
+def generate_report(values_path, tests_path, report_path):
+    with open(values_path, 'r') as values_file:
         values_data = json.load(values_file)
-        update_values(tests_data, values_data)
 
-except IOError:
-    print('Файл не найден!')
+    with open(tests_path, 'r') as tests_file:
+        tests_data = json.load(tests_file)
 
-with open('report.json', 'w') as report_file:
-    json.dump(tests_data, report_file, indent=4)
+    def fill_values(tests, values):
+        for test in tests:
+            for value in values['values']:
+                if value['id'] == test['id']:
+                    test['value'] = value['value']
+                if 'values' in test:
+                    fill_values(test['values'], values)
+
+    fill_values(tests_data['tests'], values_data)
+
+    with open(report_path, 'w') as report_file:
+        json.dump(tests_data, report_file, indent=4)
+
+
+# values_path = ''
+# tests_path = ''
+# report_path = ''
+
+values_path = input('Введите путь к файлу значений(values.json): ')
+tests_path = input('Введите путь к файлу тестов(tests.json): ')
+report_path = input('Введите путь к файлу отчета (report.json): ')
+
+generate_report(values_path, tests_path, report_path)
--- task4/task_4.py
+++ task4/task_4.py
@@ -1,6 +1,3 @@
-import numpy as np
-
-
 def main():
     file_path = input('Введите путь к файлу: ')
 
